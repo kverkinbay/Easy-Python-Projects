23295379dcc2581eae36045d4b7aa9c93a9b69c3
kverkinbay <erkinbay.ttg@gmail.com>
2025-04-19 23:41:35 +0500

    E-P-P

---
+++ lesson_num1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 19 23:21:26 2025
+
+@author: kverkinbay
+project name: lesson_num1
+"""
+
+print("Hello World!")
---
+++ lesson_num2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 19 23:26:57 2025
+
+@author: kverkinbay 
+project name: lesson_num2
+"""
+
+n=input("What is your name?:  ")
+y = input("How old are you?:  ")
+# print(n.capitalize(),",Ooo beautiful name!")
+
+print("Your name is:",n,";\ your old is:",y)
+
